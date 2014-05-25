18a97bfeeec44de9c6fab753b38db8086a8736c6
Britney Lyons <blyons333@gmail.com>
2014-05-25 12:59:03 -0500

    Postgres working?

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -13,18 +13,18 @@ post '/verify-login' do
 	pass = params[:password]
 	user = params[:username]
 
-	# if User.authenticate(user, pass)
-	# 	session[:user_id] = 1
-	# 	p session
-	# 	redirect to '/admin/homepage'
-	# else
-	# 	@error = true
-	# 	erb :login
-	# end
-	this_user = User.create(username: user)
-	this_user.password = pass
-	this_user.save
-	redirect '/'
+	if User.authenticate(user, pass)
+		session[:user_id] = 1
+		p session
+		redirect to '/admin/homepage'
+	else
+		@error = true
+		erb :login
+	end
+	# this_user = User.create(username: user)
+	# this_user.password = pass
+	# this_user.save
+	# redirect '/'
 end
 
 get '/admin/homepage' do
