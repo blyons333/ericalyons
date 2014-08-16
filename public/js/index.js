dff6e7e94b3358f398560cea02554d8dcc9bc59d
Britney Lyons <blyons333@gmail.com>
2014-08-16 14:37:36 -0500

    added header to stick on homepage, fixed side scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   setHeightsAndWidths();
   addEventListeners();
-  
+  addScrollListeners();
 });
 
 
@@ -23,7 +23,7 @@ function setHeightsAndWidths(){
   	$('.fullPageImg').width(wWidth);
 
   	var aboutSectionHeight = $('#aboutVertCenter').height();
-  	var aboutSectionPadding = (wHeight - aboutSectionHeight)/2;
+  	var aboutSectionPadding = 15 + (wHeight - aboutSectionHeight)/2;
   	// $('#aboutSection').css("padding-top", wHeight);
   	$('#aboutSection').css("padding-top", aboutSectionPadding)
   	$('#aboutSection').css("padding-bottom", aboutSectionPadding);
@@ -38,6 +38,7 @@ function addEventListeners(){
 	$("#aboutButton").click(function(event) {
 		event.preventDefault();
 	    $('html, body').animate({
+	        // scrollTop: $("#aboutVertCenter").offset().top
 	        scrollTop: $("#aboutSection").offset().top
 	    }, 600);
 	});
@@ -52,8 +53,69 @@ function addEventListeners(){
 	$("#blogButton").click(function(event) {
 		event.preventDefault();
 	    $('html, body').animate({
-	        scrollTop: $("#blogSection").offset().top
+	        scrollTop: $("#blogSection").offset().top - 55
 	    }, 600);
 	});
+}
+
+function addScrollListeners(){   
+   //Make the available tags "stick" to the top of the page
+   //as you scroll
+   $(window).scroll(updateLinkCntnrPos);
+}
+
+function updateLinkCntnrPos() {
+   
+   if ($(window).scrollTop() < wHeight-5){
+      $('#linkCntnr').css("position", "static");
+      $('#linkCntnr').css("top", "auto");
+      $('#linkCntnr').css("background-color", "");
+
+      $('#aboutButton').removeClass("content");
+      $('#contactButton').removeClass("content");
+      $('#blogButton').removeClass("content");
+      $('#aboutButton').removeClass("active");
+      $('#contactButton').removeClass("active");
+      $('#blogButton').removeClass("active");
+
+      $('#aboutButton').addClass("photo");
+      $('#contactButton').addClass("photo");
+      $('#blogButton').addClass("photo");
+
+   }else{
+
+      $('#linkCntnr').css("position", "fixed");
+      $('#linkCntnr').css("top", "0"); 
+      $('#linkCntnr').css("background-color", "white");
+      $('#linkCntnr').css("width", "100%");
+      $('#linkCntnr').css("height", "20px;");
+
+      $('#aboutButton').removeClass("photo");
+      $('#contactButton').removeClass("photo");
+      $('#blogButton').removeClass("photo");
+      $('#aboutButton').addClass("content");
+      $('#contactButton').addClass("content");
+      $('#blogButton').addClass("content");
+
+
+      if ($(window).scrollTop() >= $("#aboutSection").offset().top - 5 &&
+      		$(window).scrollTop() < $("#contactSection").offset().top) {
+      	$('#aboutButton').addClass("active");
+      	$('#contactButton').removeClass("active");
+      	$('#blogButton').removeClass("active");
+      }
+      if ($(window).scrollTop() >= $("#contactSection").offset().top - 55 && 
+      		$(window).scrollTop() < $("#blogSection").offset().top - 55) {
+      	$('#contactButton').addClass("active");
+      	$('#aboutButton').removeClass("active");
+      	$('#blogButton').removeClass("active");
+      }
+
+      if ($(window).scrollTop() >= $("#blogSection").offset().top - 55) {
+      	$('#blogButton').addClass("active");
+      	$('#contactButton').removeClass("active");
+      	$('#aboutButton').removeClass("active");
+      }
+   }
 }
 
